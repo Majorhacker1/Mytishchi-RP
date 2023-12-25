f90945b3bb8dbfa729ac66993fa923870c5c45fe
Majorhacker1 <116291812+Majorhacker1@users.noreply.github.com>
2023-12-25 17:48:48 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-// ��������� ��������� �������� �������
+// Команды меню N1
 Damage.GetContext().FriendlyFire.Value= GameMode.Parameters.GetBool("Огонь По Своим");
 Damage.GetContext().DamageOut.Value = true
 BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
@@ -8,11 +8,11 @@ Build.GetContext().FillQuad.Value = GameMode.Parameters.GetBool("FillQuad");
 Build.GetContext().RemoveQuad.Value = GameMode.Parameters.GetBool("RemoveQuad");
 Build.GetContext().FlyEnable.Value = GameMode.Parameters.GetBool("Fly");
 
-// ������ ��������� ������ ��� �����
+// Разрушаемость
 BreackGraph.BreackAll = true;
-// ���������� ���������� ������
+// Счётчик прямоугольников
 Ui.GetContext().QuadsCount.Value = false;
-// ��� ������������ �����
+// Интерфейс и функционал
 Build.GetContext().Pipette.Value = true;
 Build.GetContext().BalkLenChange.Value = false;
 Build.GetContext().SetSkyEnable.Value = false;
@@ -26,8 +26,8 @@ Build.GetContext().ChangeMapAuthorsEnable.Value = false;
 Build.GetContext().LoadMapEnable.Value = true;
 Build.GetContext().ChangeSpawnsEnable.Value = false;
 
-// Название режима
-Properties.GetContext().GameModeName.Value = "GameModes/Peace";
+// Константа
+Properties.GetContext().GameModeName.Value = "GameModes/Mytishchi RP";
 
 
 // Команды
@@ -41,9 +41,9 @@ green = GameMode.Parameters.GetBool("GreenTeam");
 
  Teams.Add("Black","Люди,вне закона", { p: 150});
 
- Teams.Add("Green","Военнослужащие",{ g: 150});	
+ Teams.Add("Green","Военнослужащие",{ g: 150});
 
-	Teams.Add("Blue", "Граждане", { b: 150 });
+ Teams.Add("Blue", "Граждане", { b: 150 });
 
 
 Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2);
